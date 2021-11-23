104c1a105c12f07ec8333c537a8e5aea5779e007
dfgs <dfgs83@gmail.com>
2021-11-23 19:20:10 +0100

    Added StreamPosConverter

---
+++ ParserLib.UnitTest/StreamPosConverterUnitTest.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Text;
+
+namespace ParserLib.UnitTest
+{
+	[TestClass]
+	public class StreamPosConverterUnitTest
+	{
+		[TestMethod]
+		public void ShouldCheckConstructor()
+		{
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new StreamPosConverter(0));
+			Assert.ThrowsException<ArgumentOutOfRangeException>(() => new StreamPosConverter(-1));
+		}
+
+		[TestMethod]
+		public void ShouldCheckParameters()
+		{
+			StreamPosConverter streamPosConverter;
+			int line, column;
+
+
+			streamPosConverter = new StreamPosConverter(1024);
+			Assert.ThrowsException<ArgumentNullException>(() => streamPosConverter.TryGetLineAndColumn(null,0,out line,out column));
+		}
+
+	
+		
+		[TestMethod]
+		public void ShouldReturnLineAndColumnAtPosition0()
+		{
+			MemoryStream stream;
+			StreamPosConverter streamPosConverter;
+			int line, column;
+			bool result;
+
+			stream = new MemoryStream(Encoding.Default.GetBytes("012\r\n345\r\n678"));
+			streamPosConverter = new StreamPosConverter(1024);
+			result = streamPosConverter.TryGetLineAndColumn(stream, 0, out line, out column);
+			Assert.IsTrue(result);
+			Assert.AreEqual(1, line);
+			Assert.AreEqual(1, column);
+		}
+		[TestMethod]
+		public void ShouldReturnLineAndColumnAtPosition1()
+		{
+			MemoryStream stream;
+			StreamPosConverter streamPosConverter;
+			int line, column;
+			bool result;
+
+			stream = new MemoryStream(Encoding.Default.GetBytes("012\r\n345\r\n678"));
+			streamPosConverter = new StreamPosConverter(1024);
+			result = streamPosConverter.TryGetLineAndColumn(stream, 1, out line, out column);
+			Assert.IsTrue(result);
+			Assert.AreEqual(1, line);
+			Assert.AreEqual(2, column);
+		}
+		[TestMethod]
+		public void ShouldReturnLineAndColumnAtPosition6()
+		{
+			MemoryStream stream;
+			StreamPosConverter streamPosConverter;
+			int line, column;
+			bool result;
+
+			stream = new MemoryStream(Encoding.Default.GetBytes("012\r\n567\r\nABC"));
+			streamPosConverter = new StreamPosConverter(1024);
+			result = streamPosConverter.TryGetLineAndColumn(stream, 6, out line, out column);
+			Assert.IsTrue(result);
+			Assert.AreEqual(2, line);
+			Assert.AreEqual(2, column);
+		}
+		[TestMethod]
+		public void ShouldReturnLineAndColumnAtPosition12()
+		{
+			MemoryStream stream;
+			StreamPosConverter streamPosConverter;
+			int line, column;
+			bool result;
+
+			stream = new MemoryStream(Encoding.Default.GetBytes("012\r\n567\r\nABC"));
+			streamPosConverter = new StreamPosConverter(1024);
+			result = streamPosConverter.TryGetLineAndColumn(stream, 12, out line, out column);
+			Assert.IsTrue(result);
+			Assert.AreEqual(3, line);
+			Assert.AreEqual(3, column);
+		}
+		[TestMethod]
+		public void ShouldNotReturnLineAndColumnAtPosition13()
+		{
+			MemoryStream stream;
+			StreamPosConverter streamPosConverter;
+			int line, column;
+			bool result;
+
+			stream = new MemoryStream(Encoding.Default.GetBytes("012\r\n567\r\nABC"));
+			streamPosConverter = new StreamPosConverter(1024);
+			result = streamPosConverter.TryGetLineAndColumn(stream, 13, out line, out column);
+			Assert.IsFalse(result);
+			Assert.AreEqual(0, line);
+			Assert.AreEqual(0, column);
+		}
+	}
+}
---
+++ ParserLib/StreamPosConverter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserLib
+{
+	public class StreamPosConverter:IStreamPosConverter
+	{
+		private int bufferSize;
+
+		public StreamPosConverter(int BufferSize)
+		{
+			if (BufferSize <= 0) throw new ArgumentOutOfRangeException(nameof(BufferSize));
+			this.bufferSize = BufferSize;
+		}
+
+		public bool TryGetLineAndColumn(Stream Stream,long Position, out int Line, out int Column)
+		{
+			long currentPos;
+			int currentLine;
+			int currentColumn;
+			byte[] buffer;
+			int count;
+
+			if (Stream == null) throw new ArgumentNullException(nameof(Stream));
+
+			buffer = new byte[bufferSize];
+			Line = 0;Column = 0;
+			currentLine = 1; currentColumn = 1;
+			 currentPos = 0;
+			try
+			{
+				Stream.Seek(0, SeekOrigin.Begin);
+				
+				while (currentPos < Stream.Length)
+				{
+
+					count=Stream.Read(buffer, 0, bufferSize);
+					for(int t=0;t<count;t++)
+					{
+						if (currentPos == Position)
+						{
+							Column = currentColumn;
+							Line = currentLine;
+							return true;
+						}
+
+						if (buffer[currentPos] == 10)
+						{
+							currentLine++;
+							currentColumn = 1;
+						}
+						else
+						{
+							currentColumn++;
+						}
+						currentPos++;
+					}
+					
+				}
+
+				return false;
+
+			}
+			catch (Exception ex)
+			{
+				return false;
+			}
+		}
+	}
+}
